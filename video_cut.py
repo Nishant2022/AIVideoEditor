26853d331b3e3b74236a2fb665460714fc9e8e65
Nishant Dash <nishant.dash@gmail.com>
2024-04-18 02:28:03 -0400

    Working Cross Fade but too much memory The current cross fade implementation uses too much memory when running with long videos and multiple segments.

--- video_cut.py
+++ video_cut.py
@@ -90,20 +90,23 @@ def make_text_intro_filter(
 
 def cross_fade(video_1: str, audio_1: str, video_2: str, audio_2: str, out_name: str, offset: int):
     command = f"[{audio_1}][{audio_2}]acrossfade=d=1[{out_name}a];"
-    command += f"[{video_1}][{video_2}]xfade=offset={offset}:transition=fade[{out_name}v]"
+    # command += f"[{video_1}][{video_2}]xfade=offset={offset}:transition=fade[{out_name}v]"
+    command += f"[{video_1}]setpts=PTS-STARTPTS[{video_1}_pts];"
+    command += f"[{video_2}]fade=in:st=0:d=1:alpha=1,setpts=PTS-STARTPTS+({offset}/TB)[{video_2}_pts];"
+    command += f"[{video_1}_pts][{video_2}_pts]overlay[{out_name}v]"
     return command
 
 
 if __name__ == '__main__':
-    # segments = [(0, 98), (145, 233),]# (310, 395), (440, 470)]
+    # segments = [(0, 98), (145, 233), (310, 395), (440, 470)]
     # video_path = "IMG_1180.MOV"
-    segments = [(20, 25)]
-    video_path = "n2.mp4"
+    # segments = [(27, 95), (106, 112)]
+    # video_path = "nishant.MP4"
+    segments = [(0, 85), (125, 137), (175, 297), (336, 410)]
+    video_path = "colin.MP4"
     cuts = video_segment_filter(segments)
-    # print(command)
-    # subprocess.call(command, shell=True)
     name = "Nishant Dash"
     division = "A1 Sparing"
-    command = f'ffmpeg -vsync 2 -i {video_path} {make_text_intro_video("30000/1001")} -filter_complex "{make_text_intro_filter(name, division)};{cuts};{cross_fade("introvid", "2", "outvtb", "outa", "final", 2)}" -map [finalv] -map [finala] -y -avoid_negative_ts make_zero out.mp4'
+    command = f'ffmpeg -hide_banner -loglevel warning -i {video_path} {make_text_intro_video("30")} -filter_complex "{make_text_intro_filter(name, division)};{cuts};{cross_fade("introvid", "2", "outvtb", "outa", "final", 2)}" -map [finalv] -map [finala] -y -avoid_negative_ts make_zero out.mp4'
     print(command)
     subprocess.call(command, shell=True)
